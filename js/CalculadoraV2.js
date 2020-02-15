07fa52c2d9e32102982afc8d6afe98925db0df8b
Bruno Weiler Foletto <brunoweiller@hotmail.com>
2020-02-14 23:11:48 -0200

    Starting to make the operations

--- js/CalculadoraV2.js
+++ js/CalculadoraV2.js
@@ -16,14 +16,35 @@ class CalculadoraV2 {
 
   action(event) {
     this.event = event;
-    if (this.event.target.value == '+'
-      || this.event.target.value == '-'
-      || this.event.target.value == '*'
-      || this.event.target.value == '/') {
-      this.destroy();
+    const evValue = this.event.target.value
+    if (evValue == '+'
+      || evValue == '-'
+      || evValue == '*'
+      || evValue == '/') {
+      this.operationPress();
+      this.setValueById();
     } else {
       this.numberPress();
       this.setValueById();
+    }
+  }
+
+  operationPress() {
+    const evValue = this.event.target.value;
+    switch (evValue) {
+      case '-':
+        this.minusOp();
+        break;
+      case '+':
+        break;
+      case '*':
+        break;
+      case '/':
+        break;
+      case '.':
+        break;
+      case '=':
+        break;
     }
   }
 
@@ -38,6 +59,28 @@ class CalculadoraV2 {
       this.value = this.value + this.event.target.value;
     } else {
       alert('CalculadoraV2/numberPress');
+    }
+  }
+
+  isNull(value) {
+    return value == ''
+      || value.size == 0
+      || value == undefined
+      || value == null;
+  }
+
+  exchangeValues() {
+    this.aux = this.value;
+    this.value = 0;
+  }
+  //todo
+  minusOp() {
+    if (!this.isNull(this.op)) {
+      this.op = '-';
+      this.exchangeValues();
+    } else {
+      this.value = this.aux - this.value;
+      this.aux = '';
     }
   }
 
